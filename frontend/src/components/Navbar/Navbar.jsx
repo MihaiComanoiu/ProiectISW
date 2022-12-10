719e91b61c2005121d48af3795633a516311d858
irinamorel <59010869+irinamorel@users.noreply.github.com>
2022-12-10 14:22:24 +0200

    navbar-initial commit

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,14 +1,39 @@
 import React from 'react';
-import {GitHamburgerMenu } from 'react-icons/gi';
+import {GiHamburgerMenu } from 'react-icons/gi';
 import {MdOutlineRestaurantMenu} from 'react-icons/md';
+
 import images from '../../constants/images';
 
 import './Navbar.css';
 
-const Navbar = () => (
-  <nav className="app_navbar">
-    Navbar
-  </nav>
+const Navbar = () =>(
+  <nav className="app__navbar">
+    <div className="app__navbar-logo">
+      <img src={images.gericht} alt="app logo" />
+    </div>
+    <ul className="app__navbar-links">
+      <li className="p__opensans"><a href="#home">Home</a></li>
+      <li className="p__opensans"><a href="#about">About</a></li>
+      <li className="p__opensans"><a href="#Menu">Menu</a></li>
+      <li className="p__opensans"><a href="#awards">Awards</a></li>
+      <li className="p__opensans"><a href="#contact">Contact</a></li>
+    </ul>
+    <div className="app__navbar-login">
+      <a href="#login" className="p__opensans">Login/Register</a>
+      <div />
+      <a href="/" className="p__opensans">Book Table</a>
+    </div>
+    <div className="app__navbar-smallscreen">
+      <GiHamburgerMenu color="#fff" fontSize={27} onClick={() => {}}/>
+      <ul className="app__navbar-smallscreen-links">
+      <li className="p__opensans"><a href="#home">Home</a></li>
+      <li className="p__opensans"><a href="#about">About</a></li>
+      <li className="p__opensans"><a href="#Menu">Menu</a></li>
+      <li className="p__opensans"><a href="#awards">Awards</a></li>
+      <li className="p__opensans"><a href="#contact">Contact</a></li>
+    </ul>
+    </div>
+ </nav>
 );
 
 export default Navbar;
