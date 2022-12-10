8de46f784812be71fb80e910cdd371d43dfe0cc6
irinamorel <59010869+irinamorel@users.noreply.github.com>
2022-12-10 12:20:59 +0200

    frontend setup and navbar

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import {GitHamburgerMenu } from 'react-icons/gi';
+import {MdOutlineRestaurantMenu} from 'react-icons/md';
+import images from '../../constants/images';
 
 import './Navbar.css';
 
 const Navbar = () => (
-  <div>
+  <nav className="app_navbar">
     Navbar
-  </div>
+  </nav>
 );
 
 export default Navbar;
