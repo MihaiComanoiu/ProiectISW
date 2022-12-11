96712ab3048e41e6852f012be4bb5b8ab3124393
irinamorel <59010869+irinamorel@users.noreply.github.com>
2022-12-11 08:04:43 +0200

    header first commit

--- frontend/src/container/Header/Header.jsx
+++ frontend/src/container/Header/Header.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
-
+import { SubHeading } from '../../components';
+import { images } from '../../constants';
 import './Header.css';
 
 const Header = () => (
-  <div>
-    Header
+  <div className="app_header app_wrapper section_padding" id="home">
+    <div className="app__wrapper_info">
+      <SubHeading tittle="Chase the new flavour"/>
+      <h1 className="app__header-h1">The Key to Fine Dining</h1>
+    </div>
+    <div className="app__wrapper_img">
+
+    </div>
+
   </div>
 );
 
