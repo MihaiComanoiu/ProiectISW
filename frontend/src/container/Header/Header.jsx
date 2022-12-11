5c4d8b8cfabb9414c48ffcd5cba98a593c286cb7
irinamorel <59010869+irinamorel@users.noreply.github.com>
2022-12-11 08:33:23 +0200

    header second commit

--- frontend/src/container/Header/Header.jsx
+++ frontend/src/container/Header/Header.jsx
@@ -8,8 +8,10 @@ const Header = () => (
     <div className="app__wrapper_info">
       <SubHeading tittle="Chase the new flavour"/>
       <h1 className="app__header-h1">The Key to Fine Dining</h1>
+      <p className="p__opensans" style={{margin: '2rem 0'}}>Sit Tellus Lobortis Sed Senectus Vivamus Molestie. Condimentum Volutpat Morbi Facilisis Quam Scelerisque Sapien. Et, Penatibus Aliquam Amet Tellus</p>
     </div>
     <div className="app__wrapper_img">
+    
 
     </div>
 
