4f6498bdde5099f617dcf5058ccdd632ef2c928e
irinamorel <59010869+irinamorel@users.noreply.github.com>
2023-01-07 16:06:50 +0200

    Intro second commit

--- frontend/src/container/Intro/Intro.jsx
+++ frontend/src/container/Intro/Intro.jsx
@@ -32,6 +32,9 @@ const Intro = () => {
           className="app__video-overlay_circle flex__center"
           onClick={handleVideo}
           >
+          {playVideo 
+          ? <BsPauseFill color="#fff" fontSize={30} /> 
+          : <BsFillPlayFill color="#fff" fontSize={30} />}
 
 
         </div>
