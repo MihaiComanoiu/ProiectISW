2a249b39c3ab7c5e4b993cc8d1c3bd0f1d50803b
irinamorel <59010869+irinamorel@users.noreply.github.com>
2023-01-07 12:27:42 +0200

    modify message header

--- frontend/src/container/Header/Header.jsx
+++ frontend/src/container/Header/Header.jsx
@@ -8,7 +8,8 @@ const Header = () => (
     <div className="app__wrapper_info">
       <SubHeading tittle="Chase the new flavour"/>
       <h1 className="app__header-h1">The Key to Fine Dining</h1>
-      <p className="p__opensans" style={{margin: '2rem 0'}}>Sit Tellus Lobortis Sed Senectus Vivamus Molestie. Condimentum Volutpat Morbi Facilisis Quam Scelerisque Sapien. Et, Penatibus Aliquam Amet Tellus</p>
+      <p className="p__opensans" style={{margin: '2rem 0'}}>
+Welcome to our restaurant! We hope you will be satisfied with our services and we wish you with all our heart "ENJOY YOUR MEAL!"</p>
       <button type="button" className="custom__button">Explore Menu</button>
     </div>
     <div className="app__wrapper_img">
