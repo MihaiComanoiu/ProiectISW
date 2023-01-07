3b0c48792fa939e4f3158d83a3e288a9ee3979da
irinamorel <59010869+irinamorel@users.noreply.github.com>
2023-01-07 17:36:41 +0200

    Laurels second commit

--- frontend/src/container/Laurels/Laurels.jsx
+++ frontend/src/container/Laurels/Laurels.jsx
@@ -7,7 +7,7 @@ import { images, data } from '../../constants';
 import './Laurels.css';
 
 const AwardCard = ({ award: {imgUrl, title, subtitle} }) => (
-  <div className="app__laurels_awards_card">
+  <div className="app__laurels_awards-card">
     <img src={imgUrl} alt="award" />
     <div className="app__laurels_awards-card_content">
       <p className="p_cormorant" style={{color: '#DCCA87'}}>{title}</p>
@@ -26,7 +26,6 @@ const Laurels = () => (
       
       <div className="app__laurels_awards">
         {data.awards.map((award) => <AwardCard award={award} key={award.title} />)}
-
       </div>
     </div>
 
