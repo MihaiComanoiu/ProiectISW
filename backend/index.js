0292039a6d686bbb191f7bd56c87023718744e4a
Comanoiu Mihai <mihaicomanoiu2017@gmail.com>
2023-01-11 13:17:11 +0200

    Added all routes to index file

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,12 @@ const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 const dotenv = require('dotenv').config()
+const userRoute = require('./routes/user')
+const authRoute = require('./routes/auth')
+const productRoute = require('./routes/product')
+const cartRoute = require('./routes/cart')
+const orderRoute = require('./routes/order')
+const stripeRoute = require('./routes/stripe')
 
 mongoose
   .connect(process.env.MONGO_URL)
@@ -11,6 +17,12 @@ mongoose
 
 app.use(express.json())
 app.use(cors())
+app.use('/api/users', userRoute)
+app.use('/api/auth', authRoute)
+app.use('/api/products', productRoute)
+app.use('/api/carts', cartRoute)
+app.use('/api/orders', orderRoute)
+app.use('/api/checkout', stripeRoute)
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT || 5000, () => console.log(`Server running on PORT ${PORT}`))
