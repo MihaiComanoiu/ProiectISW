f3a5c5349a4e963a98624137b0b3e8fc90b528ee
gabyfeier <gaby_feier@yahoo.com>
2023-01-18 16:33:52 +0200

    Updated link for menu

--- frontend/src/container/Header/Header.jsx
+++ frontend/src/container/Header/Header.jsx
@@ -3,6 +3,8 @@ import { SubHeading } from '../../components';
 import { images } from '../../constants';
 import './Header.css';
 
+import {Link} from 'react-router-dom'
+
 const Header = () => (
   <div className="app__header app__wrapper section__padding" id="home">
     <div className="app__wrapper_info">
@@ -10,7 +12,9 @@ const Header = () => (
       <h1 className="app__header-h1">The Key to Fine Dining</h1>
       <p className="p__opensans" style={{margin: '2rem 0'}}>
 Welcome to our restaurant! We hope you will be satisfied with our services and we wish you with all our heart "ENJOY YOUR MEAL!"</p>
-      <button type="button" className="custom__button">Explore Menu</button>
+      <Link to="/menu">
+        <button type="button" className="custom__button">Explore Menu</button>
+      </Link>
     </div>
     <div className="app__wrapper_img">
       <img src={images.welcome} alt="header img" />
