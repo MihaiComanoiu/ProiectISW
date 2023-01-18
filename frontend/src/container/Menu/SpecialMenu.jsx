04d01c13eb3934db324193b5a04bb7df151e284f
gabyfeier <gaby_feier@yahoo.com>
2023-01-18 16:32:07 +0200

    Updated HomePage Menu

--- frontend/src/container/Menu/SpecialMenu.jsx
+++ frontend/src/container/Menu/SpecialMenu.jsx
@@ -1,43 +1,73 @@
-import React from 'react';
+import React from 'react'
 
-import { SubHeading, MenuItem } from '../../components';
-import { images, data } from '../../constants';
-import './SpecialMenu.css';
+import { SubHeading, MenuItem } from '../../components'
+import { images, data } from '../../constants'
+import './SpecialMenu.css'
 
-const SpecialMenu = () => (
-  <div className="app__specialMenu flex__center section__padding" id="menu">
-    <div className= "app__specialMenu-title">
-      <SubHeading tittle="Menu that fits you palatte"/>
-      <h1 className="headtext__cormorant">Today's Special</h1>
-    </div>
+import { useState, useEffect } from 'react'
 
-    <div className="app__specialMenu-menu">
-      <div className="app__specialMenu-menu_wine flex__center">
-        <p className= "app__specialMenu-menu_heading">Wine & Beer</p>
-        <div className = "app__specialMenu_menu_items">
-          {data.wines.map((wine, index) => (
-            <MenuItem key={wine.title + index} title={wine.title} price={wine.price} tags={wine.tags} />
-          ))}
+import { Link } from 'react-router-dom'
+
+import axios from 'axios'
+
+const SpecialMenu = () => {
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    const getProducts = async () => {
+      try {
+        const res = await axios.get('http://localhost:1999/api/products')
+        setProducts(res.data)
+      } catch (err) {}
+    }
+    getProducts()
+  }, [])
+  return (
+    <div className='app__specialMenu flex__center section__padding' id='menu'>
+      <div className='app__specialMenu-title'>
+        <SubHeading tittle='Menu that fits you palatte' />
+        <h1 className='headtext__cormorant'>Today's Special</h1>
+      </div>
+
+      <div className='app__specialMenu-menu'>
+        <div className='app__specialMenu-menu_wine flex__center'>
+          <div className='app__specialMenu_menu_items'>
+            {products.slice(0, 4).map((product, index) => (
+              <MenuItem
+                key={product.title + index}
+                title={product.title}
+                price={product.price}
+                tags={product.tags}
+              />
+            ))}
+          </div>
+        </div>
+        <div className='app__specialMenu-menu_img'>
+          <img src={images.menu} alt='menu img' />
+        </div>
+
+        <div className='app__specialMenu-menu_cocktails flex__center'>
+          <div className='app__specialMenu_menu_items'>
+            {products.slice(4, 8).map((product, index) => (
+              <MenuItem
+                key={product.title + index}
+                title={product.title}
+                price={product.price}
+                tags={product.tags}
+              />
+            ))}
+          </div>
         </div>
       </div>
-      <div className="app__specialMenu-menu_img">
-        <img src={images.menu} alt="menu img" />
+      <div style={{ marginTop: '15px' }}>
+        <Link to='/menu'>
+          <button type='button' className='custom__button'>
+            View More
+          </button>
+        </Link>
       </div>
-
-      <div className="app__specialMenu-menu_cocktails flex__center">
-        <p className= "app__specialMenu-menu_heading">Cocktails</p>
-        <div className = "app__specialMenu_menu_items">
-          {data.cocktails.map((cocktail, index) => (
-               <MenuItem key={cocktail.title + index} title={cocktail.title} price={cocktail.price} tags={cocktail.tags} />
-          ))}
-        </div>
-      </div>
-
     </div>
-    <div style={{marginTop: '15px'}}>
-      <button type="button" className="custom__button">View More</button>
-    </div>
-  </div>
-);
+  )
+}
 
-export default SpecialMenu;
+export default SpecialMenu
